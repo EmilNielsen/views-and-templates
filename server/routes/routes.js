7afb5750bfb4a7947669a946d2b481658692f4e3
Emil Nielsen <342703@RTS-365.dk>
2019-05-27 11:28:55 +0200

    :fire:

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,15 +1,19 @@
 module.exports = (app) => {
    app.get('/', (req, res, next) => {
       res.render('home', {
-         "title": "yeetus"
+         "title": "Home"
       });
    });
 
    app.get('/product', (req, res, next) => {
-      res.render('product');
+      res.render('product', {
+         "title": "Products"
+      });
    });
 
    app.get('/contact', (req, res, next) => {
-      res.render('contact');
+      res.render('contact', {
+         "title": "Contact"
+      });
    });
 };
