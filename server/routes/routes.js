22147d97097af3c7550c1e61334fd05a08ab0251
Emil Nielsen <342703@RTS-365.dk>
2019-05-28 08:44:44 +0200

    menu highlight

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,22 +1,28 @@
 module.exports = (app) => {
    app.get('/', (req, res, next) => {
       res.render('home', {
-         "title": "Home"
+         "title": "Home",
+         "page": "home"
       });
+      console.log("Homepage loaded");
       // res.send("yote");
       // console.log("Home page"); //Udskriver i terminal
    });
 
    app.get('/product', (req, res, next) => {
       res.render('product', {
-         "title": "Products"
+         "title": "Products",
+         "page": "product"
       });
+      console.log("Productspage loaded");
    });
 
    app.get('/contact', (req, res, next) => {
       res.render('contact', {
-         "title": "Contact"
+         "title": "Contact",
+         "page": "contact"
       });
+      console.log("Contactspage loaded");
    });
 
    app.get('/test', (req, res, next) => {
